6c78bcc068dece38d1a8daa7c080119ecfe18a5e
Abraham-Jerin-John <abrahamkalarickal98@gmail.com>
2022-11-21 12:20:28 +0530

    input

---
+++ numbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class numbers {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int a,b,c;
+        System.out.println("Enter 1-Largest of 3 numbers \n 2-Smallest of 3 numbers \n 3-Prime or not\n 4-Even or not\n 5-Reverse of a number\n6-Exit");
+    }
+
+
+}
